881f028668f5beb332888f1a40299f25c197518b
HudsonPowers <89892390+HudsonPowers@users.noreply.github.com>
2022-01-17 21:34:45 -0800

    attempt for hw

--- Program.cs
+++ Program.cs
@@ -6,28 +6,32 @@ namespace DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            char ch;
-            ch = 'A';
-            Console.WriteLine($"ch is storing the value {ch}");
+            // char ch;
+            // ch = 'A';
+            // Console.WriteLine($"ch is storing the value {ch}");
 
-            int ch_decimal;
-            ch_decimal = ch;
-            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
+            // int ch_decimal;
+            // ch_decimal = ch;
+            // Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
 
-            int toConvert;
-            Console.Write("Enter an integer: ");
-            toConvert = int.Parse(Console.ReadLine());
+            // int toConvert;
+            // Console.Write("Enter an integer: ");
+            // toConvert = int.Parse(Console.ReadLine());
 
-            char asChar;
-            asChar = (char)toConvert;
-            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+            // char asChar;
+            // asChar = (char)toConvert;
+            // Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
 
-            char b;
-            b = (char)('A' + 1);
-            Console.WriteLine($"'A' + 1 = '{b}'");
+            // char b;
+            // b = (char)('A' + 1);
+            // Console.WriteLine($"'A' + 1 = '{b}'");
 
-            
+            Cipher cipher;
+            cipher = new Cipher(5);
 
+            string encrypted;
+            encrypted = cipher.Encrypt("rosebud");
+            Console.WriteLine($"The encrypted message is: '{encrypted}'");
         }
     }
 }
